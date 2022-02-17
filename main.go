43775bcbbc2663955b52adf51d4d24abd7028ed8
Ara-Chobanyan <arachobanyan2001@gmail.com>
2022-02-17 12:58:42 -0600

    Added a find and findall feature

--- main.go
+++ main.go
@@ -18,31 +18,40 @@ const (
 
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s sslmode=disable", host, port, user, password)
-	// db, err := sql.Open("postgres", psqlInfo)
-	// must(err)
-	// err = students.ResetDB(db, dbname)
-	// must(err)
-	// db.Close()
+	db, err := sql.Open("postgres", psqlInfo)
+	must(err)
+	err = students.ResetDB(db, dbname)
+	must(err)
+	db.Close()
 
 	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
-  db, err := sql.Open("postgres", psqlInfo)
+	db, err = sql.Open("postgres", psqlInfo)
 	must(err)
 	defer db.Close()
 
 	must(students.CreateClassTable(db))
-	id, err := students.InsertStudent(db, "Jon", 78.88)
+
+	id, err := students.InsertStudent(db, "Ara", 88.99)
 	must(err)
-	fmt.Println("id=", id)
+	_, err = students.InsertStudent(db, "Jon", 99.81)
+	must(err)
+	_, err = students.InsertStudent(db, "Steve", 76.58)
+	must(err)
+
+	student, err := students.GetStudent(db, id)
+	must(err)
+	fmt.Println("Student grade:", student)
+
+	allStudents, err := students.AllStudents(db)
+	must(err)
+	for _, p := range allStudents {
+		fmt.Printf("%+v\n", p)
+	}
 }
 
+// must - Small util tool to panic errors
 func must(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
-
-/*
-We want to have a new database made
-every time the program starts again.
-- [x] Create a bare bones talbe
-*/
--- students/studentdb.go
+++ students/studentdb.go
@@ -1,7 +1,16 @@
 package students
 
-import "database/sql"
+import (
+	"database/sql"
+)
 
+type Student struct {
+	id    int
+	name  string
+	grade float32
+}
+
+// createDB - Creates a new PostgreSQL database
 func createDB(db *sql.DB, name string) error {
 	_, err := db.Exec("CREATE DATABASE " + name)
 	if err != nil {
@@ -10,6 +19,41 @@ func createDB(db *sql.DB, name string) error {
 	return nil
 }
 
+// GetStudent - Finds a single student with there name and grade
+func GetStudent(db *sql.DB, id int) (*Student, error) {
+	var s Student
+	row := db.QueryRow("SELECT * FROM class_records WHERE id=$1", id)
+	err := row.Scan(&s.id, &s.name, &s.grade)
+	if err != nil {
+		return nil, err
+	}
+
+	return &s, nil
+}
+
+func AllStudents(db *sql.DB) ([]Student, error) {
+	rows, err := db.Query("SELECT * FROM class_records")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var ret []Student
+	for rows.Next() {
+		var s Student
+
+		if err := rows.Scan(&s.id, &s.name, &s.grade); err != nil {
+			return nil, err
+		}
+		ret = append(ret, s)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return ret, nil
+}
+
+// InsertStudent - Creates a new student into the class_records table
 func InsertStudent(db *sql.DB, name string, grade float32) (int, error) {
 	statement := `INSERT INTO class_records(name,average) VALUES($1, $2) RETURNING id`
 	var id int
@@ -20,6 +64,7 @@ func InsertStudent(db *sql.DB, name string, grade float32) (int, error) {
 	return id, nil
 }
 
+// ResetDB - Drops the database and creates a new one if one already exists
 func ResetDB(db *sql.DB, name string) error {
 	_, err := db.Exec("DROP DATABASE IF EXISTS " + name)
 	if err != nil {
@@ -28,6 +73,7 @@ func ResetDB(db *sql.DB, name string) error {
 	return createDB(db, name)
 }
 
+// CreateClassTable - Creates a class_records tables to store students name and average grade
 func CreateClassTable(db *sql.DB) error {
 	statement := `
   CREATE TABLE IF NOT EXISTS class_records(
